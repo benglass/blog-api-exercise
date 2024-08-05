1983c30157c9460f4e975dd7b551f1836d741e02
Ben Glassman <ben@myti.com>
2024-08-05 18:05:20 -0400

    Define a post type

--- src/posts/PostService.ts
+++ src/posts/PostService.ts
@@ -1,16 +1,24 @@
 import { v4 } from "uuid";
 
+type Post = {
+  id: string;
+  title: string;
+  contents: string;
+  timeStamp: string;
+  categoryId: number;
+};
+
 export class PostService {
-  async getPosts() {
+  async getPosts(): Promise<Post[]> {
     return [
       {
         id: v4(),
         title: "My excellent blog post",
         contents: "<p>Some HTML contents</p>",
         // TODO: Need to ensure server is using a reliable time zone (utc)
-        timestamp: new Date().toString(),
+        timeStamp: new Date().toString(),
         // TODO: Where to persist categories?
-        category: "General",
+        categoryId: 1,
       },
     ];
   }
