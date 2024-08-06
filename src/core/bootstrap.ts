76a5ec50869b27f0d6992d7957bc16a45d7894f0
Ben Glassman <ben@myti.com>
2024-08-05 20:34:15 -0400

    Add error handler

--- src/core/bootstrap.ts
+++ src/core/bootstrap.ts
@@ -1,16 +1,38 @@
-import express, { Express } from "express";
+import express, { Express, Request, Response } from "express";
 import { PostRouter } from "../posts";
 import { CategoryRouter } from "../categories";
+import { APIError } from "./APIError";
 
 export function bootstrap(): Express {
   const app: Express = express();
   const port = process.env.PORT || 8000;
 
+  // middleware
   app.use(express.json());
 
+  // routes
   const routers = [new PostRouter(), new CategoryRouter()];
   routers.forEach((router) => {
     router.apply(app);
+  });
+
+  // error handler
+  app.use((err: any, _req: Request, res: Response, _next: any) => {
+    let statusCode = 500;
+    let message = "Server error";
+
+    if (err instanceof APIError) {
+      statusCode = err.statusCode;
+      message = err.message;
+    }
+
+    res
+      .status(statusCode)
+      .json({
+        statusCode,
+        message,
+      })
+      .send();
   });
 
   app.listen(port, () => {
